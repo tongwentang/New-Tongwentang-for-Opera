8bf704bc2008175aef0c12aa6e695325194d5149
lichuan <lichuan@0ad5d002-eaed-0f45-ba62-3101f2398a6f>
2011-09-16 01:21:13 +0000

    1. 新增圖示功能設定 2. 新增自動轉換設定
    git-svn-id: svn://localhost/TongWenOpera/trunk@10 0ad5d002-eaed-0f45-ba62-3101f2398a6f

--- includes/tongwen.js
+++ includes/tongwen.js
@@ -1,32 +1,19 @@
-opera.extension.onmessage = function(event) {
-	switch (event.data) {
-		case "btnAuto":
-			TongWen.transAuto(window.top.document);
-			break;
+window.addEventListener('DOMContentLoaded', function(event) {
+	opera.extension.onmessage = function(event) {
+		switch (event.data) {
+			case "btnAuto":
+				TongWen.transAuto(document);
+				break;
 
-		case "btnTrad":
-			TongWen.trans2Trad(window.top.document);
-			break;
+			case "btnTrad":
+				TongWen.trans2Trad(document);
+				break;
 
-		case "btnSimp":
-			TongWen.trans2Simp(window.top.document);
-			break;
-
-		case "doAuto":
-			opera.postError("doAuto");
-			// TongWen.trans2Simp();
-			TongWen.transAuto(window.top.document);
-			// TongWen.transAuto(document);
-			break;
-	}
-	// alert(opera.extension.window);
-	/*
-  // Get content of incoming message.
-  var message  = event.data;
-  opera.postError("Background process sent: " + message);
-
-  //  Replies back to background script.
-  var reply = "Background process's message only had " + (message ? message.length : 0) + " characters.";
-  event.source.postMessage(reply);
-	 */
-};
+			case "btnSimp":
+				TongWen.trans2Simp(document);
+				break;
+		}
+	};
+	
+	opera.extension.postMessage('Loaded');
+}, false);
